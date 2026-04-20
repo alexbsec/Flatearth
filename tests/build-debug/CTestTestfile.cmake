d5a456e9dcb988da6152311261ec1ba8e21a090e
alexbsec <j.a.c.buschinelli@gmail.com>
2026-04-20 12:40:49 -0300

    updated gitignore

--- tests/build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexbsec/Projects/FlatearthEngine/tests
-# Build directory: /home/alexbsec/Projects/FlatearthEngine/tests/build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[flatearth_tests]=] "/home/alexbsec/Projects/FlatearthEngine/tests/build-debug/flatearth_tests")
-set_tests_properties([=[flatearth_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/alexbsec/Projects/FlatearthEngine/tests/CMakeLists.txt;123;add_test;/home/alexbsec/Projects/FlatearthEngine/tests/CMakeLists.txt;0;")
