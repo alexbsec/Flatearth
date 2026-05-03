805e63467c9785511256762c7f92323198661d67
alexbsec <j.a.c.buschinelli@gmail.com>
2026-05-03 01:33:08 -0300

    started def of ui button

---
+++ engine/src/UI/Components/UIButton.hpp
@@ -0,0 +1,10 @@
+#ifndef _FLATEARTH_ENGINE_UI_COMPONENTS_UI_BUTTON_HPP
+#define _FLATEARTH_ENGINE_UI_COMPONENTS_UI_BUTTON_HPP
+
+namespace flatearth::ui {
+
+struct UIButton {};
+
+} // namespace flatearth::ui
+
+#endif // _FLATEARTH_ENGINE_UI_COMPONENTS_UI_BUTTON_HPP
